1f9c0e39770698ed686ccb801258c4787173b577
Momen Amin <55571518+Momen-Amin@users.noreply.github.com>
2026-01-30 23:52:32 +0200

    Create clear-history.sh

---
+++ clear-history.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# ====================================
+# Linux Clear History
+# GitHub-ready version
+# ====================================
+
+history -c
+history -w
+
+echo " "
+echo " "
+echo "------->> Clearing Command History..."
+echo " "
+echo " "
+echo "######################################################"
+echo "#                                                    #"
+echo "#          History cleared successfully              #"
+echo "#                                                    #"
+echo "######################################################"
+echo " "
