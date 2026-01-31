ee1b91d95fb54ed6205abb6f3f88469ad4df3d42
Momen Amin <55571518+Momen-Amin@users.noreply.github.com>
2026-01-31 02:09:30 +0200

    Update Clear-History.sh

--- Clear-History.sh
+++ Clear-History.sh
@@ -18,7 +18,7 @@ echo "#          History cleared successfully              #"
 echo "#                                                    #"
 echo "######################################################"
 echo " "
-rm -r Clear-History.sh
+rm -f Clear-History.sh
 echo " "
 echo "######################################################"
 echo "#                                                    #"
